0b929da7a73cbd98b1c1f1aac460e34e16ca01c0
D <1356007565@qq.com>
2019-10-29 21:46:32 +0800

    优化

--- src/design/StateManager.js
+++ src/design/StateManager.js
@@ -1,43 +1,43 @@
 /* 
-状态<=>控制 条件配置化 
+状态<=>结果 配置化 
 场景: 
-  当业务复杂,一个功能是否可用或一个UI元素的显示控制或逻辑操作,需要多个状态/条件值来确定,
-  而大量存在该场景时,若每次都条件来组合判断,冗余且逻辑散落未集中管理,导致开发、维护困难 
+  当业务复杂,一个功能是否可用或一个UI元素的显示控制或逻辑操作,需要多个状态值来确定, 
+  而大量存在该场景时,若每次都用条件来组合判断,冗余且逻辑散落未集中管理,导致开发、维护困难 
   
-  场景需求: 多对多, 多个条件对多个操作 
-  对象存储: 一对多, 多个条件合并为一个对应多个操作 
-  对象查询: 多对一, 多个条件合并为一个查询指定一个操作 
 解决方案: 
-  将需要组合的条件配置化,集中管理,
-  通过一函数传入条件来进行判断, 
+  定义: 多个状态值 和 多个结果值, 配置对应关系到一列表集合中 
+  查询: 将当前状态值和定义的状态值匹配,返回结果值 
 */
 
-
 class StateManager {
-  constructor(conditionList) {
+  /* 初始化,列出将会进行判断的所有状态 */
+  constructor(stateList) {
+    // 所有可能的状态场景,未枚举的将被忽略 
+    this._stateList = stateList;  // ['st1', 'st2', ]
     
-    // 配置表,用于存储条件对应的操作 
-    this._conditionHandleMap = {
-      // 'aa:02,bb:03,cc:*,dd:*,ee:!01': {
-      //   hd1: true, 
-      //   hd2(arg){}, 
+    // 状态结果数据中心: 用于判断状态对应的结果 
+    this._StateResultList = [
+      // {
+      //   states: {
+      //     st1: 'a',
+      //     st2: 'b',
+      //   },
+      //   results: {
+      //     rst1: 'A',
+      //     rst2: 'B',
+      //   },
       // },
-    },
+    ]
     
-    // 所有可能的判断条件,未枚举的将被忽略  
-    this._conditionList = conditionList;
-    
-    // 针对于使用vue的项目,若使用了vuex,则可将该功能集成到 store 中 
+    // 针对使用了vuex插件的vue项目,可将该功能集成到 store 中 
     let that = this;
     this._store = {
-      state: {
-        conditionHandleMap: that._conditionHandleMap,
-      },
+      state: { __sttrst__: that._StateResultList, },
       getters: {
         // 通过可传参的getters来获取 条件对应的操作 
-        query({conditionHandleMap},getters){
-          return function(handle,conditionMap){
-            return that.getter(handle,conditionMap)
+        query({__sttrst__},getters){
+          return function(rst,stateMap){
+            return that.getter(rst,stateMap)
           }
         },
       },
@@ -45,25 +45,44 @@ class StateManager {
     }
   }
   
-  // 将对象转换为字符串,定义 key,使用同一的字符长度 
-  _getKey(conditionMap){
-    return this._conditionList.reduce((retVal,itm)=>{ 
-      let val = conditionMap[itm]?conditionMap[itm]:'*'; 
-      if (val instanceof Array) { val = val.join('/') }
-      return  retVal+itm+":"+val+','
-    },'').slice(0,-1) 
+  /* 单条定义状态及结果 */
+  setter(stateMap,resultMap){
+    /* 
+      设定状态时, 
+      *   表示任意 
+      !   取反 
+      []  多个条件 
+      Example: 
+        {
+          st1: '*',       // 表示任意状态值都可,默认:不填写的字段都为'*',
+          st2: '!1'       // 取反,不为'1',都可以 
+          st3: ['1','2'], // 状态值可为'1'、'2' 
+        }
+    */
+    this._StateResultList.push({ states: stateMap, results: resultMap, })
   }
-  // 将字符串还原为对象 
-  _getObj(key){
-    let obj = {};
-    key.split(',').forEach((itm,idx )=>{
-      let arr = itm.split(':');
-      obj[arr[0]] = arr[1];
+  /* 多条定义状态及结果 */
+  setConfigs(configList){
+    configList.forEach((itm,idx)=>{
+      this.setter(itm[0],itm[1]);
     });
-    return obj;
   }
+  
+  /* 查询 状态对应的结果 */
+  getter(rst,stateMap){
+    let rstMap = this._StateResultList.find(itm=>{ 
+      return this._objInclude(itm.states,stateMap) 
+    }) || {results:{},};
+    return rstMap.results[rst];
+  }
+  
+  /* 使用vuex时,接入store中 */
+  install(store,mdName='stateManager'){
+    store.registerModule(mdName,this._store)
+  }
+  
   // 条件集合包含关系判断: 条件少包含条件多 
-  _objIncludes(pObj,cObj){
+  _objInclude(pObj,cObj){
     // { key1:1, } 相当于 { key1:1, key2: <*>, } 包含 { key1: 1, key2: 2, } 
     let bol = true; // 空对象{} 包含任意对象  
     
@@ -112,45 +131,9 @@ class StateManager {
     
     return bol;
   }
-  
-  /* 初始化操作条件配置: 
-  当情况过于复杂,如判断的条件可能个数不同,导致手动定义不便,而采用函数来简化 
-  */
-  // 单条设置 
-  setter(conditionMap,handleMap){
-    let key = this._getKey(conditionMap);
-    this._conditionHandleMap[key] = { ...handleMap, };
-  }
-  // 集中设置 
-  setConfigs(configList){
-    configList.forEach((itm,idx)=>{
-      this.setter(itm[0],itm[1]);
-    });
-  }
-  
-  // 查询 条件对应的操作的值  
-  getter(handle,conditionMap){
-    let result = undefined;
-    for(var key in this._conditionHandleMap){
-      let val = this._conditionHandleMap[key];
-      let obj = this._getObj(key)
-      if ( this._objIncludes(obj,conditionMap) ) {
-        result = val[handle];
-        break;
-      }
-    };
-    return result;
-  }
-  
-  install(store,mdName='stateManager'){
-    store.registerModule(mdName,this._store)
-  }
 }
 
 
 export default StateManager;
 
 
-/* TODO: 
-  条件添加功能 [], 表示条件的多个值 
-*/
