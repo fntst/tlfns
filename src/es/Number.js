40d16fb96f16e0605d7cd6bf6652b34f6fa13306
D <1356007565@qq.com>
2019-11-24 21:38:20 +0800

    修改

--- src/es/Number.js
+++ src/es/Number.js
@@ -10,49 +10,203 @@ export function roundFloat(num,decimals=0){
 }
 
 
-// 数值精确相加 
-export function preciseAdd(arg1,arg2){ 
-  /*原理: 整数可进行精确加减计算  
-  * Input: 
-  -  PS: 做减法 需将对应的加数取负 
-  -  arg1  num,加数 
-  -  arg2  num,加数
-  * Output: num,精确相加后的数值 
-  */
-
-  let r1, r2 
-  try { r1 = arg1.toString().split(".")[1].length }
-  catch(e) { r1 = 0 } 
-  try { r2 = arg2.toString().split(".")[1].length }
-  catch(e) { r2 = 0 } 
-  
-  let m1 = Math.max(r1,r2) 
-  let m2 = Math.pow(10,m1) 
-  
-  return (arg1*m2 + arg2*m2) / m2
+/* 精确四则运算 */
+// 获取数值的小数位个数 
+function getDecimal(num){
+  let arr = num.toString().split('.')
+  let result = 0; 
+  if (arr[1]) { result = arr[1].length; }
+  return result; 
 }
-// 数值精确相乘 
-export function preciseMul(arg1,arg2,arg3){  
-  /*原理: 整数可进行精确相乘计算 
-  * Input: 
-  -  arg1   num,乘数 
-  -  arg2   num,乘数 
-  -  arg3?  num,保留小数的位数,默认: 两个乘数小数位的和 
-  * Output: num,相乘后的数值 
+/* 
+方式1: 转换为整数后运算 
+缺点: 可能导致大整数溢出问题 
+*/
+export function preciseCalc1(type,num1,num2,decimal){
+  /* 
+  type     KW,运算类型: '+' '-' '*' '/' 
+  num1     num,操作数1 
+  num2     num,操作数2 
+  decimal  int,保留小数位,默认:2 
   */
-  let r1 = arg1.toString() 
-  let r2 = arg2.toString() 
-  let len1 = r1.split(".")[1] ? r1.split(".")[1].length : 0
-  let len2 = r2.split(".")[1] ? r2.split(".")[1].length : 0 
-  let num1 = Number(r1.replace(".", ""));
-  let num2 = Number(r2.replace(".", ""));
-  let pow = Math.pow(10,len1+len2);
-  let decimals = arguments[2] ? arguments[2] : len1 + len2;
+  let typMap = { 
+    '+': function(n1,n2,pow1,pow2){ 
+      let pow = Math.max(pow1,pow2)
+      let p = Math.pow(10,pow)
+      return (n1*p+n2*p)/p;
+    }, 
+    '-': function(n1,n2,pow1,pow2){ 
+      let pow = Math.max(pow1,pow2)
+      let p = Math.pow(10,pow)
+      return (n1*p-n2*p)/p;
+    }, 
+    '*': function(n1,n2,pow1,pow2){ 
+      let p1 = Math.pow(10,pow1)
+      let p2 = Math.pow(10,pow2)
+      return ((n1*p1)*(n2*p2))/p1/p2;
+    }, 
+    '/': function(n1,n2,pow1,pow2){ 
+      let p1 = Math.pow(10,pow1)
+      let p2 = Math.pow(10,pow2)
+      return ((n1*p1)/(n2*p2))*p2/p1;
+    },
+  }
+  if (!(type in typMap)) { return console.error('运算符错误: ', type); }
   
-  return Number( (num1*num2/pow).toFixed(decimals) ) 
+  let d1 = getDecimal(num1)
+  let d2 = getDecimal(num2)
+  let deci = decimal?decimal:2;
+  return Number( typMap[type](num1,num2,d1,d2).toFixed(deci) ) 
+}
+/* 
+方式2: 转换成数组进行模拟运算  
+*/
+// 单位数乘多位数 
+function multiply(arr1,arr2,d1,d2){
+}
+export function preciseCalc2(type,num1,num2,decimal){
+  /* 
+  type     KW,运算类型: '+' '-' '*' '/' 
+  num1     num,操作数1 
+  num2     num,操作数2 
+  decimal  int,保留小数位,默认:2 
+  */
+  let typMap = { 
+    '+': function(arr1,arr2,d1,d2){ 
+      if (!arr1.isNegative && !arr2.isNegative) {
+        let d = Math.max(d1,d2)
+        let isUp = false; 
+        let result = arr1.map((itm,idx)=>{
+          let val = itm*1; 
+          let itm2 = arr2[idx]*1;
+          if (itm2) { val += itm2; }
+          if (isUp) { 
+            val++; 
+            isUp = false;
+          }
+          if (val>9) {
+            val -= 10; 
+            if (arr1[idx+1]!==undefined) { isUp = true; }
+            else { arr1.push(1) }
+          }
+          return val; 
+        }).reverse() 
+        if (d!==0) { result.splice(-d,0,'.') }
+        return result.reduce((retVal,itm,idx)=>{ 
+          return  retVal+itm;
+        },'') *1 
+      }
+      else if (arr1.isNegative && arr2.isNegative) {
+        arr1.isNegative = false; 
+        arr2.isNegative = false; 
+        return this['+'](arr1,arr2,d1,d2) * -1;
+      }
+      else if (arr1.isNegative) {
+        arr1.isNegative = false; 
+        arr2.isNegative = false; 
+        return this['-'](arr2,arr1,d1,d2);
+      }
+      else {
+        arr1.isNegative = false; 
+        arr2.isNegative = false; 
+        return this['-'](arr1,arr2,d1,d2);
+      }
+    }, 
+    '-': function(arr1,arr2,d1,d2){ 
+      if (!arr1.isNegative && !arr2.isNegative) {
+        let d = Math.max(d1,d2)
+        let isDown = false; 
+        let result = arr1.map((itm,idx)=>{
+          let val = itm*1; 
+          let itm2 = arr2[idx]*1;
+          if (itm2) { val -= itm2; }
+          if (isDown) { 
+            val--; 
+            isDown = false;
+          }
+          if (val<0) {
+            val += 10; 
+            if (arr1[idx+1]!==undefined) { isDown = true; }
+            else { arr1.push(-1) }
+          }
+          return val; 
+        }).reverse() 
+        if (d!==0) { result.splice(-d,0,'.') }
+        result = result.reduce((retVal,itm,idx)=>{ 
+          return  retVal+itm;
+        },'') 
+        if (arr1.pop()<0) {
+          result = result - Math.pow(10,result.split('.')[0].length)
+        }
+        return result*1; 
+      }
+      else if (arr1.isNegative && arr2.isNegative) {
+        arr1.isNegative = false; 
+        arr2.isNegative = false; 
+        return this['-'](arr2,arr1,d1,d2);
+      }
+      else if (arr1.isNegative) {
+        arr1.isNegative = false; 
+        arr2.isNegative = false; 
+        return this['+'](arr1,arr2,d1,d2) *-1;
+      }
+      else {
+        arr1.isNegative = false; 
+        arr2.isNegative = false; 
+        return this['+'](arr1,arr2,d1,d2);
+      }
+    }, 
+    '*': function(arr1,arr2,d1,d2){ 
+      // 
+    }, 
+    '/': function(arr1,arr2,d1,d2){ 
+      
+    },
+  }
+  if (!(type in typMap)) { return console.error('运算符错误: ', type); }
+  
+  // 
+  let d1 = getDecimal(num1); 
+  let d2 = getDecimal(num2); 
+  
+  let arr1 = num1.toString().replace(".", "").split('').reverse()
+  if (num1<0) {
+    arr1.pop()
+    arr1.isNegative = true; 
+  }
+  else {
+    arr1.isNegative = false; 
+  }
+  let arr2 = num2.toString().replace(".", "").split('').reverse()
+  if (num2<0) {
+    arr2.pop()
+    arr2.isNegative = true; 
+  }
+  else {
+    arr2.isNegative = false; 
+  }
+  let delta = d1 - d2; 
+  if (delta>0) {
+    while (delta>0) {
+      delta--;
+      arr2.unshift(0)
+    }
+  }
+  else if (delta<0) {
+    while (delta<0) {
+      delta++;
+      arr1.unshift(0)
+    }
+  }
+  
+  let deci = decimal?decimal:2;
+  return Number( typMap[type](arr1,arr2,d1,d2).toFixed(deci) ) 
 }
 
-
+/* 
+数字转换为中文 
+  如 378 叁佰柒拾捌 
+*/
 
 
 
