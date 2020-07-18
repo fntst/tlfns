86f5108d2f90202b89435c164ed8e1af95e17157
N <1356007565@qq.com>
2020-07-18 12:52:15 +0800

    优化

--- node/require_esmodule_file.js
+++ node/require_esmodule_file.js
@@ -1,13 +1,28 @@
 /* 加载 es module 文件 
 实现方法: 
   加载 ems 文件 xx.js 将 export 等关键字替换成 module.exports, 
-  然后生成 xx_.js 文件进行 require 加载返回  
+  然后生成 xx-_-.js 文件进行 require 加载返回 [可配置 .gitignore *-_-.js 来忽略缓存文件 ] 
 说明: 
   优化方案: xx_.js 文件中写入 origin_file_hash 以便缓存对比 
-  'R_ESM' 作为代码变更的表示
+  'R_ESM_F' 作为代码变更的表示
   文件加载使用同步模式,保证代码的加载优先[符合 require 语法]
 TODO: 
+  待完善所有 es module 的加载导出语法的处理 
 */
+
+// 相关 
+const config = {
+  /* 前缀和后缀不可同时为空 */
+  prefix: '',  // 转换后的文件的前缀 
+  suffix: '-_-',   // 转换后的文件的后缀 
+  check_cfg(){
+    if ( this.prefix=='' && this.suffix=='') { 
+      throw new Error(" ############# 文件名称命名存在问题请修改! ############# ") 
+    }
+  },
+}
+config.check_cfg();
+// console.log(' ################## require_esmodule_file_file ################## ');
 
 const pathMd = require("path");
 const fs = require("fs");
@@ -24,7 +39,7 @@ function dealFileData(fileStr, path){
   
   fileStr = dealImport(fileStr, path);
   
-  fileStr = `/* R_ESM+++ */ \n/* origin_file_hash:${getFileHash(fileStr)} */\n\n` + fileStr;
+  fileStr = `/* R_ESM_F+++ */ \n/* origin_file_hash:${getFileHash(fileStr)} */\n\n` + fileStr;
   return fileStr;
 } 
 function dealExport(fileStr){
@@ -33,25 +48,25 @@ function dealExport(fileStr){
   let defaultName = fileStr.match(/export\s+default\s+(?:function\s+|class\s+)(\w+).+/) || [];
   defaultName = defaultName[1]
   if (defaultName) { 
-    fileStr = fileStr.replace(/\bexport\s+default\b\s+(\w+)/msg, '/* R_ESM--- */ \n/* export default */ $1')
-    fileStr += `/* R_ESM+++ */ \nmodule.exports.default = ${defaultName};\n\n`
+    fileStr = fileStr.replace(/\bexport\s+default\b\s+(\w+)/msg, '/* R_ESM_F--- */ \n/* export default */ $1')
+    fileStr += `/* R_ESM_F+++ */ \nmodule.exports.default = ${defaultName};\n\n`
   }
   // 处理 export default xxx
   else {
     let defaultName1 = fileStr.match(/export\s+default\s+(\w+).+/) || [];
     defaultName1 = defaultName1[1]
     if (defaultName1) { 
-      fileStr = fileStr.replace(/\bexport\s+default\b\s+(\w+)\;?/msg, '/* R_ESM--- */ \n/* export default $1; */')
-      fileStr += `/* R_ESM+++ */ \nmodule.exports.default = ${defaultName1};\n\n`
+      fileStr = fileStr.replace(/\bexport\s+default\b\s+(\w+)\;?/msg, '/* R_ESM_F--- */ \n/* export default $1; */')
+      fileStr += `/* R_ESM_F+++ */ \nmodule.exports.default = ${defaultName1};\n\n`
     }
   }
   
   // 处理 export function 
   let exportFnNames = fileStr.match(/(?<=export\s+function\s+)\w+/msg);
   if ( exportFnNames && exportFnNames.length>0 ) {
-    fileStr = fileStr.replace(/\bexport\s+(?!default)/msg, '/* R_ESM--- */ \n/* export */');
+    fileStr = fileStr.replace(/\bexport\s+(?!default)/msg, '/* R_ESM_F--- */ \n/* export */');
     exportFnNames.forEach(itm=>{
-      fileStr += `/* R_ESM+++ */ \nmodule.exports.${itm} = ${itm};\n`
+      fileStr += `/* R_ESM_F+++ */ \nmodule.exports.${itm} = ${itm};\n`
     })
   }
   
@@ -59,8 +74,8 @@ function dealExport(fileStr){
   let exportName = /(?:export\s+)(\{.+?\})\;?/msg.exec(fileStr) || [];
   exportName = exportName[1]
   if ( exportName ) {
-    fileStr = fileStr.replace(/(export\s+\{.+?\}\;?)/msg, '/* R_ESM--- */ \n/* $1 */');
-    fileStr += `/* R_ESM+++ */ \nmodule.exports = ${exportName};\n`
+    fileStr = fileStr.replace(/(export\s+\{.+?\}\;?)/msg, '/* R_ESM_F--- */ \n/* $1 */');
+    fileStr += `/* R_ESM_F+++ */ \nmodule.exports = ${exportName};\n`
   }
   
   return fileStr;
@@ -71,12 +86,12 @@ function dealImport(fileStr, path){
   // 处理 import xx from "./xx.js";
   let importNames = fileStr.match(/import\s+(\w+)\s+from\s+[\"\'](.+?)[\"\']/mgs) 
   if ( importNames && importNames.length>0 ) {
-    fileStr = fileStr.replace(/(import\s+\w+\s+from\s+[\"\'].+?[\"\'])\;?/mgs,'/* R_ESM--- */ \n/* $1 */') 
+    fileStr = fileStr.replace(/(import\s+\w+\s+from\s+[\"\'].+?[\"\'])\;?/mgs,'/* R_ESM_F--- */ \n/* $1 */') 
     
     importNames.forEach(itm=>{
       let str = itm.replace(/import\s+/, '')
       let arr = str.split(/\s+from\s+/)
-      fileStr = `/* R_ESM+++ */ \nconst ${arr[0]} = require_esmodule(${arr[1]}).default;\n` + fileStr;
+      fileStr = `/* R_ESM_F+++ */ \nconst ${arr[0]} = require_esmodule_file(${arr[1]}).default;\n` + fileStr;
     })
     
     isNeedREM = true; 
@@ -85,23 +100,23 @@ function dealImport(fileStr, path){
   // 处理 import "./xx.js";
   let imports = fileStr.match(/import\s+[\"\'](.+?)[\"\']/mgs) 
   if ( imports && imports.length>0 ) {
-    fileStr = fileStr.replace(/(import\s+[\"\'].+?[\"\'])\;/mgs,'/* R_ESM--- */ \n/* $1 */') 
+    fileStr = fileStr.replace(/(import\s+[\"\'].+?[\"\'])\;/mgs,'/* R_ESM_F--- */ \n/* $1 */') 
     
     imports.forEach(itm=>{
       let str = itm.replace(/import\s+/, '')
-      fileStr = `/* R_ESM+++ */ \nrequire_esmodule(${str}).default;\n` + fileStr;
+      fileStr = `/* R_ESM_F+++ */ \nrequire_esmodule_file(${str}).default;\n` + fileStr;
     })
     
     isNeedREM = true; 
   }
 
-  // 引入 require_esmodule 处理递归 es module 的引入问题 
+  // 引入 require_esmodule_file 处理递归 es module 的引入问题 
   if (isNeedREM) {
     let relativePath = pathMd.relative(path, __dirname)
     relativePath = relativePath.replace(/\\/g,'/'); 
     relativePath = relativePath.slice(1)
-    relativePath = `${relativePath}/require_esmodule.js`
-    fileStr = `/* R_ESM+++ */ \nconst require_esmodule = require("${relativePath}")(__dirname);\n\n\n` + fileStr;
+    relativePath = `${relativePath}/require_esmodule_file.js`
+    fileStr = `/* R_ESM_F+++ */ \nconst require_esmodule_file = require("${relativePath}")(__dirname);\n\n\n` + fileStr;
   }
   
   return fileStr;
@@ -111,7 +126,7 @@ module.exports = function(dirname){
   return function (path){
     path = pathMd.resolve(dirname, path)
     let pathObj = pathMd.parse(path);
-    let newPath = `${pathObj.dir}\\_${pathObj.name}${pathObj.ext}`;
+    let newPath = `${pathObj.dir}\\${config.prefix}${pathObj.name}${config.suffix}${pathObj.ext}`;
     
     let fileDataStr = fs.readFileSync(path, { encoding: 'utf-8', })
     if ( !fs.existsSync(newPath) ) {
@@ -136,8 +151,8 @@ module.exports = function(dirname){
 }
 
 /* 使用:
-  const require_esmodule = require("../require_esmodule.js")(__dirname)
-  const xxx = require_esmodule('../xx/xxx.js')
+  const require_esmodule_file = require("../require_esmodule_file.js")(__dirname)
+  const xxx = require_esmodule_file('../xx/xxx.js')
 */
 
 
--- node/zzz_tst/fs.js
+++ node/zzz_tst/fs.js
@@ -1,53 +1,53 @@
 
 
-const require_esmodule = require("../require_esmodule.js")(__dirname)
+const require_esmodule_file = require("../require_esmodule_file.js")(__dirname)
 
 
-// require_esmodule('../../Base/check/type.js').test()
-// 
-// require_esmodule('../../Base/check/members.js').test()
-// 
-// require_esmodule('../../Base/check/regexp.js').test()
-// 
-// require_esmodule('../../Base/format/money.js').test()
-// 
-// require_esmodule('../../Base/format/query.js').test()
-// 
-// require_esmodule('../../Base/format/sort.js').test()
-// 
-// require_esmodule('../../Base/format/time.js').test()
-// 
-// require_esmodule('../../Base/func/arithmetic.js').test()
-// 
-// require_esmodule('../../Base/func/log.js').test()
-// 
-// require_esmodule('../../Base/func/StateManager.js').test()
-// 
-// require_esmodule('../../Base/func/utils.js').test()
-// 
-// require_esmodule('../../Base/functions/debounce.js').test()
-// 
-// require_esmodule('../../Base/functions/getCallerName.js').test()
-// 
-// require_esmodule('../../Base/functions/throttle.js').test()
-// 
-// require_esmodule('../../Base/functions/timeout.js').test()
-// 
-// require_esmodule('../../Base/noexport/Object.js').test()
-// 
-// require_esmodule('../../Base/polyfill/apply.js').test()
-// 
-// require_esmodule('../../Base/polyfill/bind.js').test()
-// 
-// require_esmodule('../../Base/polyfill/call.js').test()
-// 
-// require_esmodule('../../Base/callthen/Callthen.js').test()
-// 
-// require_esmodule('../../Base/callthen/Pledge.js').test()
-// 
-// require_esmodule('../../Base/callthen/Chain.js').test()
+require_esmodule_file('../../Base/check/type.js').test()
 
-require_esmodule('../../Base/callthen/index.js')
+require_esmodule_file('../../Base/check/members.js').test()
+
+require_esmodule_file('../../Base/check/regexp.js').test()
+
+require_esmodule_file('../../Base/format/money.js').test()
+
+require_esmodule_file('../../Base/format/query.js').test()
+
+require_esmodule_file('../../Base/format/sort.js').test()
+
+require_esmodule_file('../../Base/format/time.js').test()
+
+require_esmodule_file('../../Base/func/arithmetic.js').test()
+
+require_esmodule_file('../../Base/func/log.js').test()
+
+require_esmodule_file('../../Base/func/StateManager.js').test()
+
+require_esmodule_file('../../Base/func/utils.js').test()
+
+require_esmodule_file('../../Base/functions/debounce.js').test()
+
+require_esmodule_file('../../Base/functions/getCallerName.js').test()
+
+require_esmodule_file('../../Base/functions/throttle.js').test()
+
+require_esmodule_file('../../Base/functions/timeout.js').test()
+
+require_esmodule_file('../../Base/noexport/Object.js').test()
+
+require_esmodule_file('../../Base/polyfill/apply.js').test()
+
+require_esmodule_file('../../Base/polyfill/bind.js').test()
+
+require_esmodule_file('../../Base/polyfill/call.js').test()
+
+require_esmodule_file('../../Base/callthen/Callthen.js').test()
+
+require_esmodule_file('../../Base/callthen/Pledge.js').test()
+
+require_esmodule_file('../../Base/callthen/Chain.js').test()
+
+require_esmodule_file('../../Base/callthen/index.js')
 
 
 
