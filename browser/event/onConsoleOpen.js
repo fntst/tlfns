b01450152c55e0322923497fc2404a2b69c9d419
N <1356007565@qq.com>
2020-09-19 22:21:03 +0800

    1

--- browser/event/onConsoleOpen.js
+++ browser/event/onConsoleOpen.js
@@ -3,13 +3,13 @@
 
 
 // 原理1: 控制台访问元素时,控制台会获取元素的id值 
-function way1(cb,timeout){
+function way1(cb,interval){
   var div = document.createElement('div');
   Object.defineProperty(div,"id", {
     get(){ cb() },
   });
   // 使用轮训来监测 [权宜之计] 
-  return setInterval(()=>{ console.log(div); },timeout);
+  return setInterval(()=>{ console.log(div); },interval);
 }
 
 // 原理2: 控制台打开时,console的方法才会执行,且打印方法有运行较慢 
@@ -19,10 +19,15 @@ function way2(cb){
   if (performance.now() - startTime > 100){ cb() }
 }
 
-// todo 
-function way3(){ }
+// 原理3: 通过 mousemove 事件来优化间时调用 todo 
+function way3(cb){ 
+  window.addEventListener("mousemove",function(evt){
+    
+  })
+}
 
-let listenConsoleOpen = function(callback,timeout=300,way=1){
+/* 控制台打开时持续触发 */
+export default function listenConsoleOpen(callback, interval=300, way=1){
   let map = {
     1: way1,
     // 2: way2,
@@ -30,11 +35,44 @@ let listenConsoleOpen = function(callback,timeout=300,way=1){
   let fn = map[way];
   if (!fn) { throw new Error('参数错误!') };
   
-  return fn(callback,timeout);
+  return fn(callback,interval);
   // eval(`way${way}(${callback})`)
 };
 
-export default listenConsoleOpen 
+/* 控制台打开时触发 */
+export function onConsoleOpen(callback){
+  function checkOpen(){ }
+  checkOpen.toString = function(){ 
+    console.log('onConsoleOpen_run');
+    // 避免首次加载完执行,等待打开控制台时执行[第二次] [兼容性问题?] 
+    if (checkOpen.flg) { callback(); }
+    checkOpen.flg = true;
+    // if (checkOpen.flg && !checkOpen.isRunned) { 
+    //   callback(); 
+    //   checkOpen.isRunned = true;
+    // }
+  };
+  // console.log("%s", checkOpen);
+  console.log( checkOpen );
+}
+
+/* 控制台关闭时触发 todo */
+export function onConsoleClose(callback){
+  function checkOpen(){ }
+  checkOpen.toString = function(){ 
+    console.log('onConsoleOpen_run');
+    // 避免首次加载完执行,等待打开控制台时执行[第二次] [兼容性问题?] 
+    if (checkOpen.flg) { callback(); }
+    checkOpen.flg = true;
+    // if (checkOpen.flg && !checkOpen.isRunned) { 
+      //   callback(); 
+      //   checkOpen.isRunned = true;
+      // }
+    };
+    // console.log("%s", checkOpen);
+    console.log( checkOpen );
+  }
+
 
 
 
@@ -49,4 +87,9 @@ export function test(){
     console.log('clearInterval');
     clearInterval(id)
   },2000)
+  
+  onConsoleOpen(function(){
+    console.log('running');
+  })
 } 
+
