251981e99e7d2ab977c9e3b07d86bd45838d10f0
N <1356007565@qq.com>
2021-02-28 18:33:58 +0800

    增加功能模块 针对react

---
+++ react/myState.js
@@ -0,0 +1,53 @@
+/* 供未支持 useState 的版本使用 useSate 的使用方式 
+  
+  
+  // 1 安装 
+  import { installState, } from "./myState.js";
+  installState(App, document.querySelector("#app"));
+  // 2 使用 
+  import { myState, } from "./myState.js";
+  function Cpt(){
+    const [val, valSet] = myState(0);
+    
+    return <div />;
+  } 
+*/
+import ReactDOM from 'react-dom';
+let update = ()=>{ console.log('请先安装后再使用'); }
+export function installState(Root, rootEl){
+  update = ()=>{ ReactDOM.render(<Root />, rootEl) }
+} 
+function state(){
+  let _currentIdx = 0;
+  let _valList = [ ];
+   
+  function setVal(val, idx){
+    _valList[idx] = val;
+    update();
+  }
+  return function(val){
+    if (_valList.length===0) {
+      let idx = _currentIdx;
+      _currentIdx = _currentIdx + 1; 
+      setTimeout(()=>{ _valList.push(val); })
+      
+      return [
+        val, 
+        (val)=>{ setVal(val, idx) }
+      ];
+    }
+    
+    if (_currentIdx===_valList.length) {
+      _currentIdx = 0;
+    }
+    let idx = _currentIdx;
+    _currentIdx++;
+    return [
+      _valList[idx], 
+      (val)=>{ setVal(val, idx) }
+    ];
+  };
+}
+export let myState = state();
+
+
